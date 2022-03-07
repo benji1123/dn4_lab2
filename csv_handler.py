3138305713a2869228e97835f1f1489452eea841
Benjamin Li <benji112358@gmail.com>
2022-03-07 13:35:19 -0500

    add all grades

--- csv_handler.py
+++ csv_handler.py
@@ -3,8 +3,11 @@ import pandas as pd
 
 DEBUG = False
 DATA_FILE = "data.csv" 
+GRADE_INDEX_START = 3
+GRADE_INDEX_END = 12
+
 '''
- k: category-names used in client msg
+ k: category-names used in client msg (i.e. GL1A, GE4G)
  v: category-names used in csv file
 '''
 COL_NAMES = {
@@ -24,6 +27,7 @@ ID_INDEX = 1
 class csv_handler:
 	def __init__(self):
 		self.data = pd.read_csv(DATA_FILE, header=0)
+		print(self.get_all_grades("1891352"))
 
 	def get_category_mean(self, category):
 		if category in COL_NAMES:
@@ -34,6 +38,15 @@ class csv_handler:
 			if (str(row[ID_INDEX]) == stud_num):
 				return float(row[COL_NAMES[category]])
 
+	def get_all_grades(self, stud_num):
+		for index, row in self.data.iterrows():
+			if (str(row[ID_INDEX]) == stud_num):
+				grades = row[GRADE_INDEX_START:GRADE_INDEX_END]
+				return remove_last_line_from_string(str(grades))
+
+
+def remove_last_line_from_string(s):
+    return s[:s.rfind('\n')]   
 
 def test_csv_handler():
 	# runs the two getters with example params.
@@ -43,6 +56,7 @@ def test_csv_handler():
 	student1_lab1_grade = c.get_grade("1803933", "L1")
 	print(f"Student 1803933 grade for Lab 1: {student1_lab1_grade}")
 
+csv_handler()
 
 if DEBUG:	
 	test_csv_handler()
