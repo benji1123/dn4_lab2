98acb6f1447386b9c6383030f134b1195f4c696a
Benjamin Li <benji112358@gmail.com>
2022-03-07 19:31:37 -0500

    working finally

--- client_server.py
+++ client_server.py
@@ -1,22 +1,6 @@
 #!/usr/bin/python3
 
-"""
-Echo Client and Server Classes
-
-T. D. Todd
-McMaster University
-
-to create a Client: "python EchoClientServer.py -r client" 
-to create a Server: "python EchoClientServer.py -r server" 
-
-or you can import the module into another file, e.g., 
-import EchoClientServer
-e.g., then do EchoClientserver.Server()
-
-"""
-
-########################################################################
-
+from shutil import register_archive_format
 import socket
 import argparse
 import sys
@@ -26,7 +10,7 @@ from quopri import decodestring
 from csv_handler import * 
 
 HOSTNAME = "0.0.0.0"      # All interfaces.
-PORT = 5000
+PORT = 8000
 SOCKET_ADDRESS = (HOSTNAME, PORT)
 
 GET_LAB_1_AVG_CMD = "GL1A"
@@ -38,31 +22,31 @@ GET_EXAM_1_AVG_CMD = "GE1A"
 GET_EXAM_2_AVG_CMD = "GE2A"
 GET_EXAM_3_AVG_CMD = "GE3A"
 GET_EXAM_4_AVG_CMD = "GE4A"
+GET_GRADES_CMD = "GG"
+
+COL_NAMES = {
+    "L1" : "Lab 1", 
+    "L2" : "Lab 2", 
+    "L3" : "Lab 3", 
+    "L4" : "Lab 4", 
+    "M" : "Midterm", 
+    "E1" : "Exam 1", 
+    "E2" : "Exam 2", 
+    "E3" : "Exam 3", 
+    "E4" : "Exam 4"
+}
+
 
 ########################################################################
 # Echo Server class
 ########################################################################
 
-class Server:
 
-    # Set the server hostname used to define the server socket address
-    # binding. Note that "0.0.0.0" or "" serves as INADDR_ANY. i.e.,
-    # bind to all local network interfaces.
-    # HOSTNAME = "192.168.1.22" # single interface
-    # HOSTNAME = "hornet"       # valid hostname (mapped to address/IF)
-    # HOSTNAME = "localhost"    # local host (mapped to local address/IF)
-    # HOSTNAME = "127.0.0.1"    # same as localhost
-    
+class Server:
     RECV_BUFFER_SIZE = 1024 # Used for recv.
     MAX_CONNECTION_BACKLOG = 10
-
-    # We are sending text strings and the encoding to bytes must be
-    # specified.
-    # MSG_ENCODING = "ascii" # ASCII text encoding.
     MSG_ENCODING = "utf-8" # Unicode text encoding.
-
-    # Create server socket address. It is a tuple containing
-    # address/hostname and port.
+    csv_handler = csv_handler()
     
     def __init__(self):
         self.create_listen_socket()
@@ -72,15 +56,8 @@ class Server:
         try:
             # Create an IPv4 TCP socket.
             self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-            # Set socket layer socket options. This one allows us to
-            # reuse the socket without waiting for any timeouts.
             self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-            # Bind socket to socket address, i.e., IP address and port.
             self.socket.bind(SOCKET_ADDRESS)
-
-            # Set socket to listen state.
             self.socket.listen(Server.MAX_CONNECTION_BACKLOG)
             print("Listening on port {} ...".format(PORT))
         except Exception as msg:
@@ -90,119 +67,98 @@ class Server:
     def process_connections_forever(self):
         try:
             while True:
-                # Block while waiting for accepting incoming TCP
-                # connections. When one is accepted, pass the new
-                # (cloned) socket info to the connection handler
-                # function. Accept returns a tuple consisting of a
-                # connection reference and the remote socket address.
                 self.connection_handler(self.socket.accept())
         except Exception as msg:
             print(msg)
         except KeyboardInterrupt:
             print()
         finally:
-            # If something bad happens, make sure that we close the
-            # socket.
             self.socket.close()
             sys.exit(1)
 
+    def handle_get_grade(self, recvd_str):
+        ID_INDEX = 0
+        PW_INDEX = 1
+        recvd_hash = recvd_str.split(" ")[1]
+        print(f"Received ID/password hash <{recvd_hash}> from client.")
+        l = self.csv_handler.get_ids_and_passwords()
+        for i in l:
+            h = str(create_hash(i[ID_INDEX], i[PW_INDEX]))
+            if h == recvd_hash:
+                send_str = self.csv_handler.get_all_grades(i[ID_INDEX])
+                return send_str
+
     def connection_handler(self, client):
-        # Unpack the client socket address tuple.
         connection, address_port = client
         print("-" * 72)
         print("Connection received from {}.".format(address_port))
         # Output the socket address.
         print(client)
 
-        c = csv_handler()
-
         while True:
             try:
-                # Receive bytes over the TCP connection. This will block
-                # until "at least 1 byte or more" is available.
                 recvd_bytes = connection.recv(Server.RECV_BUFFER_SIZE)
-            
-                # If recv returns with zero bytes, the other end of the
-                # TCP connection has closed (The other end is probably in
-                # FIN WAIT 2 and we are in CLOSE WAIT.). If so, close the
-                # server end of the connection and get the next client
-                # connection.
                 if len(recvd_bytes) == 0:
                     print("Closing client connection ... ")
                     connection.close()
                     break
                 
-                # Decode the received bytes back into strings. Then output
-                # them.
                 recvd_str = recvd_bytes.decode(Server.MSG_ENCODING)
-                print("Received: ", recvd_str)
+                print("Command received: ", recvd_str)
 
                 if recvd_str == GET_MIDTERM_AVG_CMD:
-                    send_str = c.get_category_mean("M")
+                    send_str = self.csv_handler.get_category_mean("M")
                     connection.sendall(str(send_str).encode(Server.MSG_ENCODING))
 
                 elif recvd_str == GET_LAB_1_AVG_CMD:
-                    send_str = c.get_category_mean("L1")
+                    send_str = self.csv_handler.get_category_mean("L1")
                     connection.sendall(str(send_str).encode(Server.MSG_ENCODING))
 
                 elif recvd_str == GET_LAB_2_AVG_CMD:
-                    send_str = c.get_category_mean("L2")
+                    send_str = self.csv_handler.get_category_mean("L2")
                     connection.sendall(str(send_str).encode(Server.MSG_ENCODING))
                 
                 elif recvd_str == GET_LAB_3_AVG_CMD:
-                    send_str = c.get_category_mean("L3")
+                    send_str = self.csv_handler.get_category_mean("L3")
                     connection.sendall(str(send_str).encode(Server.MSG_ENCODING))
 
                 elif recvd_str == GET_LAB_4_AVG_CMD:
-                    send_str = c.get_category_mean("L4")
+                    send_str = self.csv_handler.get_category_mean("L4")
                     connection.sendall(str(send_str).encode(Server.MSG_ENCODING))
 
                 elif recvd_str == GET_EXAM_1_AVG_CMD:
-                    send_str = c.get_category_mean("E1")
+                    send_str = self.csv_handler.get_category_mean("E1")
                     connection.sendall(str(send_str).encode(Server.MSG_ENCODING))
 
                 elif recvd_str == GET_EXAM_2_AVG_CMD:
-                    send_str = c.get_category_mean("E2")
+                    send_str = self.csv_handler.get_category_mean("E2")
                     connection.sendall(str(send_str).encode(Server.MSG_ENCODING))
 
                 elif recvd_str == GET_EXAM_3_AVG_CMD:
-                    send_str = c.get_category_mean("E3")
+                    send_str = self.csv_handler.get_category_mean("E3")
                     connection.sendall(str(send_str).encode(Server.MSG_ENCODING))
 
                 elif recvd_str == GET_EXAM_4_AVG_CMD:
-                    send_str = c.get_category_mean("E4")
+                    send_str = self.csv_handler.get_category_mean("E4")
                     connection.sendall(str(send_str).encode(Server.MSG_ENCODING))
 
-                #elif recvd_str == GET_GRADES_CMD:
-                    # need student num
-                    # send_str = c.get_all_grades()
-
-             
-
+                elif len(recvd_str) > 1 and recvd_str[0:2] == GET_GRADES_CMD:
+                    send_str = self.handle_get_grade(recvd_str)
+                    connection.sendall(str(send_str).encode(Server.MSG_ENCODING))
             except KeyboardInterrupt:
                 print()
                 print("Closing client connection ... ")
                 connection.close()
                 break
 
+
 ########################################################################
 # Echo Client class
 ########################################################################
 
+
 class Client:
-
-    # Set the server to connect to. If the server and client are running
-    # on the same machine, we can use the current hostname.
-    # SERVER_HOSTNAME = socket.gethostname()
-    # SERVER_HOSTNAME = "localhost"
-    
-    # Try connecting to the compeng4dn4 echo server. You need to change
-    # the destination port to 50007 in the connect function below.
-    # SERVER_HOSTNAME = 'compeng4dn4.mooo.com'
-
-    RECV_BUFFER_SIZE = 5 # Used for recv.    
-    # RECV_BUFFER_SIZE = 1024 # Used for recv.
-
+    RECV_BUFFER_SIZE = 128 # Used for recv.    
     def __init__(self):
         self.get_socket()
         self.connect_to_server()
@@ -210,21 +166,13 @@ class Client:
 
     def get_socket(self):
         try:
-            # Create an IPv4 TCP socket.
             self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-            # Allow us to bind to the same port right away.            
-            # self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            # Bind the client socket to a particular address/port.
-            # self.socket.bind((Client.HOSTNAME, 40000))
-                
         except Exception as msg:
             print(msg)
             sys.exit(1)
 
     def connect_to_server(self):
         try:
-            # Connect to the server using its socket address tuple.
             self.socket.connect((HOSTNAME, PORT))
             print("Connected to \"{}\" on port {}".format(HOSTNAME, PORT))
         except Exception as msg:
@@ -232,11 +180,10 @@ class Client:
             sys.exit(1)
 
     def get_console_input(self):
-        # In this version we keep prompting the user until a non-blank
-        # line is entered, i.e., ignore blank lines.
         while True:
             self.input_text = input("Input: ")
             if self.input_text != "":
+                print(f"Command entered: {self.input_text}")
                 break
     
     def send_console_input_forever(self):
@@ -248,15 +195,20 @@ class Client:
             except (KeyboardInterrupt, EOFError):
                 print()
                 print("Closing server connection ...")
-                # If we get and error or keyboard interrupt, make sure
-                # that we close the socket.
                 self.socket.close()
                 sys.exit(1)
                 
     def connection_sendall(self):
         try:
-            # Send string objects over the connection. The string must
-            # be encoded into bytes objects first.
+            if self.input_text == GET_GRADES_CMD:
+                id = getpass.getpass(prompt = "Enter student ID number: ")
+                pw = getpass.getpass(prompt = "Enter password: ")
+                print(f"ID number <{id}> and password <{pw}> received")
+                h = str(create_hash(id, pw))
+                print(f"ID/password hash <{h}> sent to server.")
+                self.input_text += " " + h
+            if self.input_text[0] == 'G' and self.input_text[-1] == 'A':
+                print(f"Fetching {self.input_text[1:-1]} grades")
             self.socket.sendall(self.input_text.encode(Server.MSG_ENCODING))
         except Exception as msg:
             print(msg)
@@ -264,28 +216,16 @@ class Client:
 
     def connection_receive(self):
         try:
-            # Receive and print out text. The received bytes objects
-            # must be decoded into string objects.
             recvd_bytes = self.socket.recv(Client.RECV_BUFFER_SIZE)
-
-            # recv will block if nothing is available. If we receive
-            # zero bytes, the connection has been closed from the
-            # other end. In that case, close the connection on this
-            # end and exit.
             if len(recvd_bytes) == 0:
                 print("Closing server connection ... ")
                 self.socket.close()
                 sys.exit(1)
-
-            print("Received: ", recvd_bytes.decode(Server.MSG_ENCODING))
-
+            print(f"Received: {recvd_bytes.decode(Server.MSG_ENCODING)}\n")
         except Exception as msg:
             print(msg)
             sys.exit(1)
 
-########################################################################
-# Authentication
-########################################################################
 
 def create_hash(id, pw):
     id = id.encode(encoding = 'UTF-8') #encode id using UTF-8
@@ -295,14 +235,6 @@ def create_hash(id, pw):
     hash.update(pw) #repeated calls are equivalent to single call with concatenation of all arguments
     return hash.digest() #return the digest of the data passed to the update()
 
-########################################################################
-# Process command line arguments if this module is run directly.
-########################################################################
-
-# When the python interpreter runs this module directly (rather than
-# importing it into another file) it sets the __name__ variable to a
-# value of "__main__". If this file is imported from another module,
-# then __name__ will be set to that module's name.
 
 if __name__ == '__main__':
     roles = {'client': Client,'server': Server}
--- csv_handler.py
+++ csv_handler.py
@@ -27,7 +27,7 @@ COL_NAMES = {
 class csv_handler:
 	def __init__(self):
 		self.data = pd.read_csv(DATA_FILE, header=0)
-		print(self.get_ids_and_passwords())
+		print(f"\n\nCSV DATA:\n{self.data}")
 
 	def get_category_mean(self, category):
 		if category in COL_NAMES:
@@ -39,10 +39,12 @@ class csv_handler:
 				return float(row[COL_NAMES[category]])
 
 	def get_all_grades(self, stud_num):
+		output = ""
 		for index, row in self.data.iterrows():
-			if (str(row[ID_INDEX]) == stud_num):
-				grades = row[GRADE_INDEX_START:GRADE_INDEX_END]
-				# last line is garbage (so remove)
+			if (str(row[ID_INDEX]) == str(stud_num)):
+				for i, grade in enumerate(row[GRADE_INDEX_START:GRADE_INDEX_END]):
+					output += f" {list(COL_NAMES.keys())[i]} : {grade}, "
+		return output[:-2]
 
 	def get_ids_and_passwords(self):
 		l = []
@@ -52,8 +54,5 @@ class csv_handler:
 			l.append((id, pw))
 		return l
 
-
 def remove_last_line_from_string(s):
     return s[:s.rfind('\n')]
-
-csv_handler()
