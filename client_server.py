df3a1c4ec45f4e628af01479963d351abc565884
Benjamin Li <benji112358@gmail.com>
2022-03-08 09:34:13 -0500

    one file

--- client_server.py
+++ client_server.py
@@ -7,12 +7,36 @@ import sys
 import getpass
 import hashlib
 from quopri import decodestring
-from csv_handler import * 
 
-HOSTNAME = "0.0.0.0"      # All interfaces.
+import csv
+import pandas as pd
+
+#CSV CONSTANTS
+DATA_FILE = "data.csv" 
+ID_INDEX = 1            # student id
+PW_INDEX = 2            # password id
+GRADE_INDEX_START = 3   # lab1
+GRADE_INDEX_END = 12    # exam4
+
+'''
+k: category-names used in client msg (i.e. GL1A, GE4G)
+v: category-names used in csv file
+'''
+COL_NAMES = {
+    "L1" : "Lab 1", 
+    "L2" : "Lab 2", 
+    "L3" : "Lab 3", 
+    "L4" : "Lab 4", 
+    "M" : "Midterm",
+    "E1" : "Exam 1", 
+    "E2" : "Exam 2", 
+    "E3" : "Exam 3", 
+    "E4" : "Exam 4"
+}
+
+HOSTNAME = "0.0.0.0"
 PORT = 8000
 SOCKET_ADDRESS = (HOSTNAME, PORT)
-
 GET_LAB_1_AVG_CMD = "GL1A"
 GET_LAB_2_AVG_CMD = "GL2A"
 GET_LAB_3_AVG_CMD = "GL3A"
@@ -24,23 +48,48 @@ GET_EXAM_3_AVG_CMD = "GE3A"
 GET_EXAM_4_AVG_CMD = "GE4A"
 GET_GRADES_CMD = "GG"
 
-COL_NAMES = {
-    "L1" : "Lab 1", 
-    "L2" : "Lab 2", 
-    "L3" : "Lab 3", 
-    "L4" : "Lab 4", 
-    "M" : "Midterm", 
-    "E1" : "Exam 1", 
-    "E2" : "Exam 2", 
-    "E3" : "Exam 3", 
-    "E4" : "Exam 4"
-}
+
+########################################################################
+# CSV HANDLING
+########################################################################
+
+class csv_handler:
+    def __init__(self):
+        self.data = pd.read_csv(DATA_FILE, header=0)
+        print(f"\n\nCSV DATA:\n{self.data}")
+
+    def get_category_mean(self, category):
+        if category in COL_NAMES:
+            return float(self.data[COL_NAMES[category]].mean())
+
+    def get_grade(self, stud_num, category):
+        for index, row in self.data.iterrows():
+            if (str(row[ID_INDEX]) == stud_num):
+                return float(row[COL_NAMES[category]])
+
+    def get_all_grades(self, stud_num):
+        output = ""
+        for index, row in self.data.iterrows():
+            if (str(row[ID_INDEX]) == str(stud_num)):
+                for i, grade in enumerate(row[GRADE_INDEX_START:GRADE_INDEX_END]):
+                    output += f" {list(COL_NAMES.keys())[i]} : {grade}, "
+        return output[:-2]
+
+    def get_ids_and_passwords(self):
+        l = []
+        for index, row in self.data.iterrows():
+            id = str(row[ID_INDEX])
+            pw = str(row[PW_INDEX])
+            l.append((id, pw))
+        return l
+
+def remove_last_line_from_string(s):
+    return s[:s.rfind('\n')]
 
 
 ########################################################################
 # Echo Server class
 ########################################################################
-
 
 class Server:
     RECV_BUFFER_SIZE = 1024 # Used for recv.
@@ -95,9 +144,7 @@ class Server:
         connection, address_port = client
         print("-" * 72)
         print("Connection received from {}.".format(address_port))
-        # Output the socket address.
         print(client)
-
         while True:
             try:
                 recvd_bytes = connection.recv(Server.RECV_BUFFER_SIZE)
